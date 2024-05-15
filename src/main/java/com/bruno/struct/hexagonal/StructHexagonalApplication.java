84324e4e12aa15897a4f5e67a40964df0d828179
Bruno Knop <81430617+Bruno-Knop@users.noreply.github.com>
2024-05-15 10:17:48 -0300

    Init

---
+++ src/main/java/com/bruno/struct/hexagonal/StructHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.struct.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StructHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bruno/struct/hexagonal/StructHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.struct.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StructHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
