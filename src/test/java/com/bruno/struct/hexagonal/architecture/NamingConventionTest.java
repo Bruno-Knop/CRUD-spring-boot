dcecc77db1b4c1855bd925c11cb10b5e1f8fa061
Bruno Knop <81430617+Bruno-Knop@users.noreply.github.com>
2024-05-16 15:06:24 -0300

    Config Architecture Test

---
+++ src/test/java/com/bruno/struct/hexagonal/architecture/NamingConventionTest.java
@@ -0,0 +1,114 @@
+package com.bruno.struct.hexagonal.architecture;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+@AnalyzeClasses(packages = "com.bruno.struct.hexagonal")
+public class NamingConventionTest {
+
+    @ArchTest
+    public static final ArchRule adapters_reside_only_adapters_package = classes()
+            .that()
+            .haveNameMatching(".*Adapter")
+            .should()
+            .resideInAnyPackage("..adapters.in", "..adapters.out");
+
+    @ArchTest
+    public static final ArchRule consumer_reside_only_consumer_package = classes()
+            .that()
+            .haveNameMatching(".*Consumer")
+            .should()
+            .resideInAPackage("..adapters.in.consumer");
+
+    @ArchTest
+    public static final ArchRule controller_reside_only_controller_package = classes()
+            .that()
+            .haveNameMatching(".*Controller")
+            .should()
+            .resideInAPackage("..adapters.in.controller");
+
+    @ArchTest
+    public static final ArchRule client_reside_only_client_package = classes()
+            .that()
+            .haveNameMatching(".*Client")
+            .should()
+            .resideInAPackage("..adapters.out.client");
+
+    @ArchTest
+    public static final ArchRule repository_reside_only_repository_package = classes()
+            .that()
+            .haveNameMatching(".*Repository")
+            .should()
+            .resideInAPackage("..adapters.out.repository");
+
+    @ArchTest
+    public static final ArchRule message_reside_only_message_package = classes()
+            .that()
+            .haveNameMatching(".*Message")
+            .should()
+            .resideInAPackage("..adapters.in.consumer.message");
+
+    @ArchTest
+    public static final ArchRule request_reside_only_request_package = classes()
+            .that()
+            .haveNameMatching(".*Request")
+            .should()
+            .resideInAPackage("..adapters.in.controller.request");
+
+    @ArchTest
+    public static final ArchRule mapper_reside_only_mapper_package = classes()
+            .that()
+            .haveNameMatching(".*Mapper")
+            .should()
+            .resideInAnyPackage(
+                    "..adapters.in.consumer.mapper",
+                    "..adapters.in.controller.mapper",
+                    "..adapters.out.client.mapper",
+                    "..adapters.out.repository.mapper"
+            );
+
+    @ArchTest
+    public static final ArchRule response_reside_only_response_package = classes()
+            .that()
+            .haveNameMatching(".*Response")
+            .should()
+            .resideInAnyPackage("..adapters.in.controller.response", "..adapters.out.client.response");
+
+    @ArchTest
+    public static final ArchRule entity_reside_only_entity_package = classes()
+            .that()
+            .haveNameMatching(".*Entity")
+            .should()
+            .resideInAPackage("..adapters.out.repository.entity");
+
+    @ArchTest
+    public static final ArchRule usecase_reside_only_usecase_package = classes()
+            .that()
+            .haveNameMatching(".*UseCase")
+            .should()
+            .resideInAPackage("..application.core.usecase");
+
+    @ArchTest
+    public static final ArchRule portsIn_reside_only_portsIn_package = classes()
+            .that()
+            .haveNameMatching(".*InputPort")
+            .should()
+            .resideInAPackage("..application.ports.in");
+
+    @ArchTest
+    public static final ArchRule portsOut_reside_only_portsOut_package = classes()
+            .that()
+            .haveNameMatching(".*OutputPort")
+            .should()
+            .resideInAPackage("..application.ports.out");
+
+    @ArchTest
+    public static final ArchRule config_reside_only_config_package = classes()
+            .that()
+            .haveNameMatching(".*Config")
+            .should()
+            .resideInAPackage("..config");
+}
